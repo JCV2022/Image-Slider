78404681cdf35606b9b87058478ff4fd9f3518c5
JCV2022 <101784278+JCV2022@users.noreply.github.com>
2023-08-28 01:28:45 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
